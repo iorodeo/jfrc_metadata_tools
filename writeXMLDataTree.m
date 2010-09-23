fc10078cdcaf537f3da5f22a79b00256f8de5bcb
Will Dickson <will@iorodeo.com>
2010-09-23 15:39:38 -0400

    More project reorganization.

--- writeXMLDataTree.m
+++
@@ -1,7 +0,0 @@
-function writeXMLDataTree(filename,tree)
-% Writes data in XMLDataNode based tree to an xml file with given filename
-%
-% Arguments:
-%  filename   = name of output file
-%  tree       = the XMLDataTree to write
-tree.write(filename);
