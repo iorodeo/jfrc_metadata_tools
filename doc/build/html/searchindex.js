4a8a7c53a110e8ffcb42978509306bc366432af5
Will Dickson <will@iorodeo.com>
2010-09-28 12:43:12 -0700

    Added empty sphinx project to documentation folder which will be used for creating the documention for metadata tools.

---
+++ doc/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({objects:{},terms:{index:0,search:0,welcom:0,jfrc:0,tool:0,indic:0,page:0,content:0,tabl:0,document:0,modul:0,metadata:0},objtypes:{},titles:["Welcome to JFRC Metadata Tools&#8217;s documentation!"],objnames:{},filenames:["index"]})
